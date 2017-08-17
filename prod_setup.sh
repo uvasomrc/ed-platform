ff90837efc9d1d37faa6220e2fc2c1b699d0b487
Dan Funk <daniel.h.funk@gmail.com>
2017-08-17 13:16:18 -0400

    Adding production setup file, which should get executed by the production server on a git push.

---
+++ prod_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Run the basic operations to update a production setup after a code change.
+# This script should be executed by a post_recieve git hook after completing
+# a checkout.
+# --------------------------------------------------------------------------
+
+# Move the configuration file into place.
+cp /home/ubuntu/edp_config.py /var/www/edp/instance/config.py
+
+# Update the database
+source activate edp
+export APP_CONFIG_FILE="../config/default.py"
+cd backend && python manage.py clear_data
+cd backend && python manage.py db upgrade
+cd backend && python manage.py load_data
+
+# Rebuild the front end.
+cd frontend && ng build -dist
+
+# Reload apache
+sudo service apache2 reload
