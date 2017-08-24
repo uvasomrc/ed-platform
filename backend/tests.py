f83f0d164aefd967c059b5b267b18fee2c4ccdce
Dan Funk <daniel.h.funk@gmail.com>
2017-08-24 16:52:46 -0400

    Forgot tests on that last commit.

--- backend/tests.py
+++ backend/tests.py
@@ -1,7 +1,6 @@
 import unittest
 from flask import json
-from ed_platform import app,db,data_loader
-from flask_script import Manager
+from ed_platform import app, db, data_loader, models
 
 
 class TestCase(unittest.TestCase):
@@ -30,27 +29,71 @@ class TestCase(unittest.TestCase):
         print(rv.data)
         assert b'ED Platform API' in rv.data
 
-    def test_add_track(self):
-        data = {'image_file':'track_one.jpg',
-                'title':'This is the title',
-                'description':'This is the description'}
-
+    def add_test_track(self):
+        data = {'image_file': 'track_one.jpg',
+                'title': 'This is the title',
+                'description': 'This is the description'}
         rv = self.app.post('/api/track', data=json.dumps(data), follow_redirects=True,
                            content_type="application/json")
-
         rd = json.loads(rv.get_data(as_text=True))
+        return rd
+
+    def add_test_workshop(self):
+        data = {'image_file': 'workshop_one.jpg',
+                'title': 'This is a test workshop',
+                'description': 'This is the test description',
+                }
+        rv = self.app.post('/api/workshop', data=json.dumps(data), follow_redirects=True,
+                           content_type="application/json")
+        return json.loads(rv.get_data(as_text=True))
+
+    def get_workshop(self, id):
+        rv = self.app.get('/api/workshop/%id' %id,
+                           follow_redirects=True,
+                           content_type="application/json")
+        return json.loads(rv.get_data(as_text=True))
+
+
+    def test_add_track(self):
+        rd = self.add_test_track()
         assert rd['title'] == "This is the title"
         assert rd['description'] == "This is the description"
-        assert rd['image_file'] == "track_one.jpg"
         assert rd["id"] is not None
 
         rv2 = self.app.get('/api/track/' + str(rd["id"]))
-        assert b'track_one.jpg' in rv2.data
         assert b'This is the title' in rv2.data
         assert b'This is the description' in rv2.data
 
-    def test_track_has_workshops(self):
-        pass
+    def test_get_tracks(self):
+        response = self.app.get('/api/track')
+        all_tracks = json.loads(response.get_data(as_text=True))
+        assert len(all_tracks["tracks"]) > 1
+        track1 = all_tracks["tracks"][0]
+        self.assertTrue("title" in track1.keys())
+        self.assertTrue("description" in track1.keys())
+        self.assertTrue(len(track1["_links"]["workshops"]) > 0)
+
+    def test_sample_data_load(self):
+        track = models.Track.query.filter_by(id=1).first()
+        assert track.title == "Learning Python The Hard Way"
+
+    def test_add_workshop(self):
+        workshop = self.add_test_workshop()
+        self.assertEqual("This is a test workshop", workshop["title"])
+
+    def test_add_workshop_to_track(self):
+        track    = self.add_test_track()
+        workshop = self.add_test_workshop()
+        data = json.dumps({"workshops":[workshop]})
+        url = '/api/track/%i/workshops' % track['id']
+
+        response = self.app.patch(url,
+                            follow_redirects=True,
+                            data = data,
+                            content_type="application/json")
+
+        workshops = json.loads(response.get_data(as_text=True))
+        self.assertTrue(len(workshops["workshops"]), 1)
 
 if __name__ == '__main__':
     unittest.main()
