34ab86f73c03c846eaa92ca55fc8cf5f7193b0b4
Dan Funk <daniel.h.funk@gmail.com>
2017-09-06 10:29:22 -0400

    Marking attributes as not required, just to see what the heck we are getting

--- backend/config/default.py
+++ backend/config/default.py
@@ -7,10 +7,10 @@ SQLALCHEMY_DATABASE_URI = "postgresql://ed_user:ed_pass@localhost/ed_platform"
 #: Default attribute map for single signon.
 SSO_ATTRIBUTE_MAP = {
     'ADFS_AUTHLEVEL': (False, 'authlevel'),
-    'ADFS_GROUP': (True, 'group'),
-    'ADFS_LOGIN': (True, 'nickname'),
+    'ADFS_GROUP': (False, 'group'),
+    'ADFS_LOGIN': (False, 'nickname'),
     'ADFS_ROLE': (False, 'role'),
-    'ADFS_EMAIL': (True, 'email'),
+    'ADFS_EMAIL': (False, 'email'),
     'ADFS_IDENTITYCLASS': (False, 'external'),
     'HTTP_SHIB_AUTHENTICATION_METHOD': (False, 'authmethod'),
 }
