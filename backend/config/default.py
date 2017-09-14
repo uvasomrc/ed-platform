d52caa5547c6943ab6b5d1250aee804fcb2137aa
Dan Funk <daniel.h.funk@gmail.com>
2017-09-14 14:25:58 -0400

    Should not default to the development platform.

--- backend/config/default.py
+++ backend/config/default.py
@@ -1,7 +1,7 @@
 DEBUG = True
 TESTING = False
 CORS_ENABLED = False
-DEVELOPMENT = True
+DEVELOPMENT = False
 SECRET_KEY = 'ed_platform_key_of_deep_secret_knackwursts!'
 SQLALCHEMY_DATABASE_URI = "postgresql://ed_user:ed_pass@localhost/ed_platform"
 
