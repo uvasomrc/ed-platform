a1330c22e5b838b7a7f5048e5d4af0f2a3ff261c
Dan Funk <daniel.h.funk@gmail.com>
2017-10-12 14:07:46 -0400

    Trying to debug a sometimes problem with chrome that is showing up for a few people.

--- frontend/src/app/participant.ts
+++ frontend/src/app/participant.ts
@@ -13,35 +13,39 @@ export class Participant {
 
   constructor(values: Object = {}, parent = null) {
     Object.assign(this, values);
-    this.links = new Links(values['_links']);
-    if ('participant_sessions' in values) {
-    this.sessions = Array<Session>();
-    for (const ps of values['participant_sessions']) {
-      if (ps['review_score']) { this.reviews.push(new Review(ps)); }
-      if (parent == null) {
-        const session = new Session(ps['session'], this);
-        this.sessions.push(session);
-      }
+    if ('_links' in values) {
+      this.links = new Links(values['_links']);
     }
+    if ('participant_sessions' in values) {
+      this.sessions = Array<Session>();
+      for (const ps of values['participant_sessions']) {
+        if (ps['review_score']) {
+          this.reviews.push(new Review(ps));
+        }
+        if (parent == null) {
+          const session = new Session(ps['session'], this);
+          this.sessions.push(session);
+        }
+      }
     }
   }
 
   upcomingSessions(): Array<Session> {
-    return this.sessions.filter( s => {
-      return s.date_time.valueOf() >= new Date().valueOf() && s.instructors.filter( i => i.id === this.id).length === 0;
-    } );
+    return this.sessions.filter(s => {
+      return s.date_time.valueOf() >= new Date().valueOf() && s.instructors.filter(i => i.id === this.id).length === 0;
+    });
   }
 
   pastSessions(): Array<Session> {
-    return this.sessions.filter( s => {
-      return s.date_time.valueOf() < new Date().valueOf() && s.instructors.filter( i => i.id === this.id).length === 0;
-    } );
+    return this.sessions.filter(s => {
+      return s.date_time.valueOf() < new Date().valueOf() && s.instructors.filter(i => i.id === this.id).length === 0;
+    });
   }
 
   teachingSessions(): Array<Session> {
-    return this.sessions.filter( s => {
-      return s.instructors.filter( i => i.id === this.id).length > 0;
-    } );
+    return this.sessions.filter(s => {
+      return s.instructors.filter(i => i.id === this.id).length > 0;
+    });
   }
 
   isTeacher(): boolean {
