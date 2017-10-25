8edc0bc1aa868a400b25ba0e17a6da790cb883f2
Dan Funk <daniel.h.funk@gmail.com>
2017-10-25 09:37:05 -0400

    Adding the frontend account form, missed in the last commit.

---
+++ frontend/src/app/account-form/account-form.component.ts
@@ -0,0 +1,50 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AccountService} from '../account.service';
+import {Participant} from '../participant';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-account-form',
+  templateUrl: './account-form.component.html',
+  styleUrls: ['./account-form.component.css']
+})
+export class AccountFormComponent implements OnInit {
+
+  @Input('participant') participant: Participant;
+  accountForm: FormGroup;
+  displayName: FormControl;
+  bio: FormControl;
+  image: FormControl;
+
+  constructor(private accountService: AccountService, private router: Router) {
+    this.accountService.getAccount().subscribe(acct => {
+      this.participant = acct;
+    });
+  }
+
+  ngOnInit() {
+    this.displayName = new FormControl(this.participant.display_name, [Validators.required, Validators.maxLength(256)]);
+    this.bio  = new FormControl(this.participant.bio, []);
+    this.accountForm = new FormGroup({
+      displayName: this.displayName,
+      bio: this.bio
+    });
+  }
+
+  onSubmit() {
+    if (this.accountForm.valid) {
+      this.participant.display_name = this.displayName.value;
+      this.participant.bio = this.bio.value;
+      this.accountService.updatePaticipant(this.participant).subscribe( p => {
+        // If this is a new account, then redirect them to page they started on when they hit login, and
+        // subsequently got redirected here to complete their details.
+        if(this.participant.new_account) {
+          this.router.navigate([this.accountService.getRouteAfterLogin()]);
+        }
+      });
+    }
+  }
+
+
+}
