3409a39d2e130633f4f6098bfa00e0789cb72990
Dan Funk <daniel.h.funk@gmail.com>
2017-12-28 14:26:58 -0500

    A basic python command that can be cronned to send email messages, just useful for testing at the moment, but will add loops in the future

---
+++ backend/ed_manage/email_manager.py
@@ -0,0 +1,31 @@
+from flask_script import Command, Option, Manager, prompt_bool
+from flask import current_app
+from ed_platform import models
+
+#class Confirmation(Command):
+
+#    """Loops through workshops, notifying participants of various events """
+
+#    def __init__(self, app):
+#        object.__init__(self)
+#        self.notifier = Notify(app)
+
+#    def run(self):
+#        print("I'm gonna notify some folks.")
+from ed_platform import Notify
+
+
+def sub_opts(app, **kwargs):
+    pass
+manager = Manager(sub_opts, usage="Send Automated Email Messages")
+
+@manager.command
+def confirmation():
+    "Asks participants to confirm their attendance in upcoming sessions"
+    print("Sending Confirmation Emails.")
+
+    notifier = Notify(current_app)
+    participant = models.Participant.query.filter_by(uid='dhf8r').first()
+    session = models.Session.query.first()
+    notifier.message_confirm(participant, participant, session)
+
--- backend/manage.py
+++ backend/manage.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate, MigrateCommand
 
 from ed_platform import app, db, elastic_index
 from ed_manage import data_manager
+from ed_manage.email_manager import manager as email_manager
 
 
 # Load the configuration
@@ -19,6 +20,9 @@ manager.add_command('db', MigrateCommand)
 manager.add_command('load_data', data_manager.LoadData(db, "example_data.json"))
 manager.add_command('clear_data', data_manager.ClearData(db))
 manager.add_command('index_data', data_manager.IndexData(db, elastic_index))
+#manager.add_command('send_confirmation', email_manager.Confirmation(app))
+
+manager.add_command("email", email_manager)
 
 if __name__ == '__main__':
     manager.run()
