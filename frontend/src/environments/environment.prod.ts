2e69d07ad58c75f4f23221549b37d86b734d150e
Dan Funk <daniel.h.funk@gmail.com>
2018-01-17 10:09:55 -0500

    Updating production with new url.

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://edp.uvasomrc.io',
+  api: 'https://education.cadre.virginia.edu',
   ga_id: 'UA-112017665-1' // Google Analytics tag
 };
