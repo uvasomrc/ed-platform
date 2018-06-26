2ccd249587a17f803566eb0b5deb20c09b2fe38e
Dan Funk <daniel.h.funk@gmail.com>
2018-06-26 15:36:34 -0400

    Adding boto3 library to the update script.

--- prod_update.sh
+++ prod_update.sh
@@ -27,7 +27,7 @@ echo "Running from ${HOME_DIR}"
 # Continue to update conda in case I can get that working ...
 eval 'cd ${HOME_DIR}/backend && conda env update'
 # But also install via pip3, which is what the production server is using now.
-eval 'pip3 install flask sqlalchemy psycopg2 flask-migrate flask-script flask-cors flask-marshmallow flask-compress marshmallow-sqlalchemy flask-sso pyjwt flask-httpauth flask_mail elasticsearch_dsl flask-uploads python-magic requests-toolbelt icalendar markdown2'
+eval 'pip3 install flask sqlalchemy psycopg2 flask-migrate flask-script flask-cors flask-marshmallow flask-compress marshmallow-sqlalchemy flask-sso pyjwt flask-httpauth flask_mail elasticsearch_dsl flask-uploads python-magic requests-toolbelt icalendar markdown2 boto3'
 if [ "$ENV" == "staging" ]; then 
    eval 'python ${HOME_DIR}/backend/manage.py cleardb'
    eval 'python ${HOME_DIR}/backend/manage.py clearindex'
